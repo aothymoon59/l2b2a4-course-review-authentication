64c789e0ac602afdbcd04bdd907133d1253ca652
aothymoon59 <aothymoon59@gmail.com>
2023-12-31 03:23:33 +0600

    auth middleware created

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,46 @@
+import httpStatus from 'http-status';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
+import AppError from '../errors/AppError';
+import catchAsync from '../utils/catchAsync';
+import { TUserRole } from '../modules/Auth/auth.interface';
+import { User } from '../modules/User/user.model';
+import { NextFunction, Request, Response } from 'express';
+
+const auth = (...requiredRoles: TUserRole[]) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization;
+
+    // checking if the token is missing
+    if (!token) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+    }
+
+    // checking if the given token is valid
+    const decoded = jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+    ) as JwtPayload;
+
+    const { role, _id } = decoded;
+
+    // checking if the user is exist
+    const user = await User.findById(_id);
+
+    if (!user) {
+      throw new AppError(httpStatus.NOT_FOUND, 'This user is not found !');
+    }
+
+    if (requiredRoles && !requiredRoles.includes(role)) {
+      throw new AppError(
+        httpStatus.UNAUTHORIZED,
+        'You are not authorized  hi!',
+      );
+    }
+
+    req.user = decoded as JwtPayload & { role: string };
+    next();
+  });
+};
+
+export default auth;
